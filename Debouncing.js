122c5266933590bb7efd7064ab6ac541a43b809c
mohanapriya-narapa <mohanapriya.narapa@outlook.com>
2024-10-03 18:58:36 -0500

    debouncing

---
+++ Debouncing.js
@@ -0,0 +1,18 @@
+const getData =()=>
+{
+    console.log("fetching data");
+}
+
+
+const debounce=function(fn,d)
+{let timer;
+return function()
+{
+    clearTimeout(timer);
+    timer=setTimeout(()=>{
+fn();
+    },d)
+};
+}
+const betterFunction = debounce(getData, 2000);
+betterFunction();
