8029592bd68609831d97bf6f603e8e1f69c286a6
mohanapriya-narapa <mohanapriya.narapa@outlook.com>
2024-10-02 23:43:17 -0500

    binding

--- Binding.js
+++ Binding.js
@@ -7,8 +7,22 @@ const person =
     },
 }
 
-person.sayMyName();
+// person.sayMyName();
 
 // explicit binding
 
+function sayMyName()
+{
+    console.log(`my is name is ${this.name}`);
+}
+// sayMyName.call(person);
 
+//new binding
+
+function Person(name)
+{
+    this.name =name
+}
+const p1=new Person('mohana');
+const p2=new Person('priya')
+console.log(p1.name, p2.name);
