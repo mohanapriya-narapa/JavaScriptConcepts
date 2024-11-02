b4458cb5c5dcfd2c4ed9715e21e8369e859ecf88
mohanapriya-narapa <mohanapriya.narapa@outlook.com>
2024-11-02 16:24:12 -0500

    debounce

--- Debouncing.js
+++ Debouncing.js
@@ -1,21 +1,20 @@
-const getData =()=>
+const getData =(msg)=>
 {
-    console.log("fetching data");
+    console.log("fetching data", msg);
 }
 
 
 const debounce=function(fn,d)
 {let timer;
-return function()
+return function(...args)
 {
     clearTimeout(timer);
     timer=setTimeout(()=>{
-fn();
+fn.apply(this,args);
     },d)
 };
 }
 const betterFunction = debounce(getData, 2000);
-betterFunction();
-betterFunction();
+betterFunction("hello");
 
 //if difference between 2 key strokes is 300 ms then make a call
